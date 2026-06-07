00ea03cdaf9a18b6f14bc2e6e4d6f9c93c733caf
Anu Mishra <anumishra555555@gmail.com>
2026-06-07 11:48:56 +0530

    Fix merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { gsap } from "gsap"
-import {Draggable} from "gsap/Draggable"
+import { Draggable } from "gsap/all"
 import { Dock, Navbar,Welcome } from "#components" 
-<<<<<<< HEAD
 import { Terminal,Resume,Finder,Text,Image,Contact,Trash} from "#windows"
-=======
 import Terminal from "#windows/Terminal"
->>>>>>> 01971d9d1633de4bc2b4dc4911d64cf4c8640538
+
 
 
 gsap.registerPlugin(Draggable)
@@ -17,15 +15,13 @@ const App = () => {
       <Welcome />
       <Dock />
       <Terminal/>
-<<<<<<< HEAD
+
       <Resume/>
       <Finder/>
       <Text/>
       <Image/>
       <Trash/>
       <Contact/>
-=======
->>>>>>> 01971d9d1633de4bc2b4dc4911d64cf4c8640538
     </main>
   )
 }
